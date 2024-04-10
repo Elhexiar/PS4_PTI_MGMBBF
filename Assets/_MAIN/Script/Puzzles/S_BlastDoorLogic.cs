7efab061ef202fe1cdd9615ebe03b4c3f10a9da2
LeVraiSodaa <118454205+Elhexiar@users.noreply.github.com>
2024-04-10 11:22:39 +0200

    Implementation Puzzle 1 dans le _main

--- Assets/_MAIN/Script/Puzzles/S_BlastDoorLogic.cs
+++ Assets/_MAIN/Script/Puzzles/S_BlastDoorLogic.cs
@@ -6,10 +6,115 @@ namespace Assets._MAIN.Script.Puzzles
     public class S_BlastDoorLogic : MonoBehaviour
     {
 
-        
-        public void TestTopCog()
+
+        public bool movingCogOnTop = false;
+        public bool movingCogOnBot = false;
+        public bool topPathOpen = false;
+        public bool bottomPathOpen = false;
+        public bool cogInHand = false;
+        public bool isLocked = true;
+        public Animator topCogAnimator, botCogAnimator, overallDoorAnimator, middleBlockAnimator, movingCogAnimator;
+
+
+        public void TopValve()
         {
-            Debug.Log("TestTopCog !! ");
+            if (topPathOpen == false)
+            {
+                if (movingCogOnTop)
+                {
+                    topCogAnimator.Play("EngrennageTopSuccess");
+                    movingCogAnimator.Play("TopCogSuccess");
+                    topPathOpen = true;
+                }
+                else
+                {
+                    topCogAnimator.Play("EngrenageEmpty");
+                }
+            }
+        }
+
+        public void BotValve()
+        {
+            if (bottomPathOpen == false)
+            {
+                if (movingCogOnBot)
+                {
+                    botCogAnimator.Play("EngrenageBotSuccess");
+                    movingCogAnimator.Play("BotCogSuccess");
+                    bottomPathOpen = true;
+                }
+                else
+                {
+                    botCogAnimator.Play("Emptyengrenange");
+                }
+            }
+        }
+
+        public void MiddleValve()
+        {
+            if (isLocked)
+            {
+                if (topPathOpen == true && bottomPathOpen == true)
+                {
+                    middleBlockAnimator.Play("BarreFermetureOpenSuccess");
+                    isLocked = false;
+                }
+                else
+                {
+                    middleBlockAnimator.Play("BarreFermetureOpenFailure");
+                    overallDoorAnimator.Play("EchecOuverturePorte");
+                }
+            }
+            else
+            {
+                overallDoorAnimator.Play("OuverturePorte");
+            }
+
+        }
+
+        public void BottomCogPicked()
+        {
+
+            if (cogInHand == true)
+            {
+                movingCogAnimator.Play("BotCogPlaced");
+                cogInHand = false;
+                movingCogOnBot = true;
+            }
+            else
+            {
+                if (movingCogOnBot)
+                {
+                    movingCogAnimator.Play("BotCogPicked");
+                    cogInHand = true;
+                    movingCogOnBot = false;
+                }
+            }
+
+        }
+
+        public void TopCogPicked()
+        {
+            if (cogInHand == true)
+            {
+                movingCogAnimator.Play("TopCogPlaced");
+                cogInHand = false;
+                movingCogOnTop = true;
+            }
+            else
+            {
+                if (movingCogOnTop)
+                {
+                    movingCogAnimator.Play("TopCogPicked");
+                    cogInHand = true;
+                    movingCogOnTop = false;
+                }
+            }
+        }
+
+        public void Test(string anim)
+        {
+            movingCogAnimator.Play(anim);
         }
 
 
